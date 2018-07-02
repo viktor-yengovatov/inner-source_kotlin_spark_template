e8dab4df4e1176668a6a97baabf941691dd429b0
Viktor Yengovatov <Viktor_Yengovatov@rcomext.com>
2018-07-02 07:40:23 +0300

    * moving DB configuration from DataBaseClient to Context * Readme added (README.md)

--- src/main/kotlin/com/home/Context.kt
+++ src/main/kotlin/com/home/Context.kt
@@ -2,14 +2,19 @@ package com.home
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule
+import com.home.integration.DataBaseClient
 import com.home.integration.DataService
 import com.home.integration.RemoteDataClient
+import org.jooq.DSLContext
+import org.jooq.impl.DSL
 import retrofit2.Retrofit
 import retrofit2.converter.jackson.JacksonConverterFactory
+import java.sql.Connection
+import java.sql.DriverManager
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
-fun api() : Context = Context
+fun api(): Context = Context
 
 /**
  * Note that this is an `object`, so we have a guarantee that this is a Singleton
@@ -28,4 +33,20 @@ object Context {
     val dataService: DataService = retrofit.create(DataService::class.java)
 
     val remoteDataClient: RemoteDataClient = RemoteDataClient(dataService)
+
+    val dbConnection: Connection
+        get() {
+            val url = getProperty("DB_URL")
+            val password = getProperty("DB_PASSWORD")
+            val userName = getProperty("DB_USERNAME")
+
+            return DriverManager.getConnection(url, userName, password)
+        }
+
+    val jooqContext: DSLContext = DSL.using(dbConnection)
+
+    val dbClient: DataBaseClient = DataBaseClient(dbConnection);
+
+    private fun getProperty(key: String): String? =
+            System.getProperty(key) ?: io.github.cdimascio.dotenv.dotenv()[key]
 }
--- src/main/kotlin/com/home/integration/DataBaseClient.kt
+++ src/main/kotlin/com/home/integration/DataBaseClient.kt
@@ -1,14 +1,14 @@
 package com.home.integration;
 
 import com.example.jooq.tables.User
-import io.github.cdimascio.dotenv.dotenv
 import org.jooq.DSLContext
 import org.jooq.SQLDialect
 import org.jooq.impl.DSL
 import java.sql.Connection
-import java.sql.DriverManager
+import java.sql.DriverManager.getConnection
 
-class DataBaseClient {
+class DataBaseClient(val dbConnection: Connection) {
+
     fun proceedGetUsers() {
         executeQuery { dslContext ->
             val result = dslContext.select().from(User.USER).fetch()
@@ -29,18 +29,9 @@ class DataBaseClient {
     }
 
     fun executeQuery(function: (DSLContext) -> Unit) {
-        getConnection().use {
+        dbConnection.use {
             DSL.using(it, SQLDialect.POSTGRES).use(function)
         }
     }
 
-    private fun getConnection(): Connection? {
-        val dotenv = dotenv()
-
-        val url = dotenv["DB_URL"]
-        val password = dotenv["DB_PASSWORD"]
-        val userName = dotenv["DB_USERNAME"]
-
-        return DriverManager.getConnection(url, userName, password)
-    }
 }
